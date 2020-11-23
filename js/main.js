8fcdb21c6e8b8e06c3da37b2c92f46e558502692
SergeyTokareff <sergey.v.tokareff@gmail.com>
2020-11-23 14:20:49 +0200

    add slider, order

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-(function () {
+const openMenu = function () {
     const burger = document.querySelector('.hamburger');
     const fullScreenMenu = document.querySelector('.fullscreen-menu');
     const body = document.querySelector('body');
-    const topPlank = document.querySelector('.hamburger__plank--top');
-    const middlePlank = document.querySelector('.hamburger__plank--middle');
-    const bottomPlank = document.querySelector('.hamburger__plank--bottom');
-    const overlay = document.querySelector('.fullscreen-menu');
     
-    burger.addEventListener('click', function (e) {
+         
+    burger.addEventListener('click', function(e) {
         e.preventDefault();
-        fullScreenMenu.classList.toggle('active');
+        burger.classList.toggle('hamburger--active');
         body.classList.toggle('body-active-menu');
-        topPlank.classList.toggle('hamburger__plank--top-active');
-        middlePlank.classList.toggle('hamburger__plank--middle-active');
-        bottomPlank.classList.toggle('hamburger__plank--bottom-active');
-    });
-
-    overlay.addEventListener('click', function (e) {
-        e.preventDefault();
-        overlay.classList.remove('active');
-    })
-})();
+        fullScreenMenu.classList.toggle('fullscreen-menu--active');
+    });     
+}();
 
