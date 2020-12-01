061ca443add42bd7f2561daa6426c6b93dc00b6b
SergeyTokareff <sergey.v.tokareff@gmail.com>
2020-12-01 10:43:06 +0200

    add directory build

---
+++ dist/js/slider.js
@@ -0,0 +1,46 @@
+let slideActive = 1;
+let slideCount = $('#slider__items').children().length;
+let translateWidth = 0;
+
+$(document).ready(function () {
+    $('#left').click(function (e) {
+        e.preventDefault();
+        prevSlide();
+    });
+
+    $('#right').click(function (e) {
+        e.preventDefault();
+        nextSlide();
+    });
+});
+
+function nextSlide() {
+    if (slideActive == slideCount || slideActive <= 0 || slideActive > slideCount) {
+        $('#slider__items').css('transform', 'translate(0, 0)');
+        slideActive = 1;
+    } else {
+        translateWidth = -$('#slider__items').width() * slideActive;
+        $('#slider__items').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)'
+        });
+        slideActive++;
+    }    
+}
+
+function prevSlide() {
+    if (slideActive == 1 || slideActive <= 0 || slideActive > slideCount) {
+        translateWidth = -$('#slider__items').width() * (slideCount - 1);
+        $('#slider__items').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)'
+        });
+        slideActive = slideCount;
+        } else {
+            translateWidth = -$('#slider__items').width() * (slideActive - 2);
+        $('#slider__items').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)'
+        });
+            slideActive--;
+        }    
+}
+
+
---
+++ dist/js/vertical_accordion.js
@@ -0,0 +1,20 @@
+const button = document.querySelectorAll('.team__name');
+const accordion = document.querySelectorAll('.team__accordion');
+
+$(button).each(function () {
+    $(this).click(function (e) {
+        e.preventDefault();        
+        
+        if ($(accordion).not(e.target.nextElementSibling).hasClass('team__accordion--open')) {
+            $(accordion).removeClass('team__accordion--open');
+        };
+
+        $(e.target.nextElementSibling).toggleClass('team__accordion--open');
+            
+        if ($(button).not(this).hasClass('triangle-up')) {                
+            $(button).removeClass('triangle-up');
+        }        
+        
+        $(this).toggleClass('triangle-up');        
+    });    
+});
